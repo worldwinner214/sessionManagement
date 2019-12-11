084b42df0c3fabb7049b45fa7fd9a76401d8126c
vishmaj1@in.ibm.com <vishmaj1@in.ibm.com>
2019-12-11 13:23:18 +0530

    qqqqqqqqqqqqq

---
+++ src/main/java/com/techgeeknext/controller/SpringBootSessionController.java
@@ -0,0 +1,43 @@
+package com.techgeeknext.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SpringBootSessionController {
+    @PostMapping("/addNote")
+    public String addNote(@RequestParam("note") String note, HttpServletRequest request) {
+        //get the notes from request session
+        List<String> notes = (List<String>) request.getSession().getAttribute("NOTES_SESSION");
+        //check if notes is present in session or not
+        if (notes == null) {
+            notes = new ArrayList<>();
+            // if notes object is not present in session, set notes in the request session
+            request.getSession().setAttribute("NOTES_SESSION", notes);
+        }
+        notes.add(note);
+        request.getSession().setAttribute("NOTES_SESSION", notes);
+        return "redirect:/home";
+    }
+    @GetMapping("/home")
+    public String home(Model model, HttpSession session) {
+        List<String> notes = (List<String>) session.getAttribute("NOTES_SESSION");
+        model.addAttribute("notesSession", notes!=null? notes:new ArrayList<>());
+        return "home";
+    }
+    @PostMapping("/invalidate/session")
+    public String destroySession(HttpServletRequest request) {
+        //invalidate the session , this will clear the data from configured database (Mysql/redis/hazelcast)
+        request.getSession().invalidate();
+        return "redirect:/home";
+    }
+}
