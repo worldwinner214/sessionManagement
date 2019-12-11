c632c021f9c773b893007e098f99c475503a55fc
worldwinner214 <43905330+worldwinner214@users.noreply.github.com>
2019-12-11 16:23:21 +0530

    Update SpringBootSessionController.java

--- src/main/java/com/techgeeknext/controller/SpringBootSessionController.java
+++ src/main/java/com/techgeeknext/controller/SpringBootSessionController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
